8eae04525e83adf7748102f80812bcb1852fccb4
Hideyuki Utsunomiya <hidechae@gmail.com>
2024-04-14 19:18:53 +0900

    add slack message sample

--- go/slack-go/main.go
+++ go/slack-go/main.go
@@ -1,24 +1,62 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/slack-go/slack"
 )
 
+var c *slack.Client
+
 func main() {
 	token := os.Getenv("SLACK_TOKEN")
-	api := slack.New(token, slack.OptionDebug(true))
+	c = slack.New(token, slack.OptionDebug(true))
+	ctx := context.Background()
 
-	// メッセージの送信処理
-	_, _, err := api.PostMessage(
-		"#notification-test",
-		slack.MsgOptionText("This is send test. Current time is "+time.Now().Format(time.DateTime), true),
-	)
-	if err != nil {
+	//if err := SendSimpleMessage(ctx, "This is send test. Current time is "+time.Now().Format(time.DateTime)); err != nil {
+	//	fmt.Printf("%s\n", err)
+	//}
+
+	if err := SendBlockMessage(ctx, "Hello World", "This is body text."); err != nil {
 		fmt.Printf("%s\n", err)
-		return
 	}
+}
+
+func SendSimpleMessage(ctx context.Context, text string) error {
+	_, _, err := c.PostMessageContext(ctx, "#notification-test", slack.MsgOptionText(text, true))
+	return err
+}
+
+func SendBlockMessage(ctx context.Context, title, body string) error {
+	_, _, err := c.PostMessageContext(ctx, "#notification-test", slack.MsgOptionBlocks(
+		// 区切り線
+		slack.NewDividerBlock(),
+
+		// テキストのみのセクション
+		&slack.SectionBlock{
+			Type: slack.MBTSection,
+			Text: &slack.TextBlockObject{
+				Type: "mrkdwn",
+				Text: "これはタイトルです",
+			},
+		},
+
+		// テキストとフィールドのセクション
+		slack.NewSectionBlock(
+			&slack.TextBlockObject{
+				Type: "mrkdwn",
+				Text: fmt.Sprintf("*%s*", title),
+			},
+			[]*slack.TextBlockObject{
+				{
+					Type: "plain_text",
+					Text: body,
+				},
+			},
+			nil,
+		),
+	))
+	return err
 }
